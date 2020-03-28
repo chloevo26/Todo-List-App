f05059fd091bd59cc1a9ce0860a53e2f9ba2d5eb
Thu Vo <chloevo26@gmail.com>
2020-03-27 21:48:14 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+
+})
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
+// check off specific Todos by clicking
+$("ul").on("click", "li", function () {
+    // if the text is gray, turns it into black
+    // else turns it to gray with a line-through
+    // if ($(this).css("color") === "rgb(128, 128, 128)") {
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     });
+    // } else {
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });
+    // }
+
+    $(this).toggleClass("completed")
+});
+
+//click on X to delete todo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();   // this refer to li not the span
+    }); // remove the entire li
+    event.stopPropagation(); // stop the event from bubbling out to the parent 
+});
+
+$("input[type = 'text']").keypress(function (event) {
+    if (event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+
+        // create a new li and add to ul
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-pencil-alt").click(function () {
+    $("input[type ='text']").fadeToggle(500);
+});
+
