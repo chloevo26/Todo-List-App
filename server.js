d9ed9b9a94e5ce4e4d6674afb6f8c6ad8b25639a
Thu Vo <chloevo26@gmail.com>
2020-03-27 22:09:37 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-
-})
-
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
-
-
 // check off specific Todos by clicking
 $("ul").on("click", "li", function () {
     // if the text is gray, turns it into black
